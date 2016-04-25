71aeafc8c0cf82bae6912c162a5a49febbf78552
Alex Berghage <aberghage@gmail.com>
2016-04-24 20:41:58 -0700

    Added missing include to make clang on windows happy

--- common/batteries_included.h
+++ common/batteries_included.h
@@ -20,12 +20,13 @@
 #include <errno.h>
 #include <inttypes.h>
 #include <limits.h>
+#include <math.h>
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 /* # C++ Standard Library Includes */
 #include <functional>
