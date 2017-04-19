38bfdbd3a5eb43aaf8b242f08ecaf74a4c71f735
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2017-04-19 17:17:22 -0400

    Add a hack for check our LOG format strings

--- common/logging.h
+++ common/logging.h
@@ -68,9 +68,21 @@ inline i32 _logMessage(c_cstr    message,
  * -----------------------
  * If you want to log a thing, you almost 100% definitely certainly want to
  * probably use this one.
+ * If you've compiled with DEBUG active, we're not just going to munge and print
+ * your message. We're also going to run it through an `snprintf(NULL, 0, ...)`
+ * to give your compiler a change to check that your format string is correctly
+ * structured. Actually using `NULL` doesn't work here, unfortunately, so this
+ * trick will add the cost of writing `\0` into `format_verification` to every
+ * message actually printed.
  */
-#define LOG(MESSAGE, ...)                                          \
-    _logMessage(::_variadicExpand(MESSAGE, ##__VA_ARGS__).c_str(), \
-                __FILE__,                                          \
-                __LINE__,                                          \
-                __FUNCTION__)
+#if defined(DEBUG)
+    static char format_verification[1] = {0};
+    #define LOG(MESSAGE, ...)                                          \
+        snprintf(format_verification, 0, MESSAGE, ##__VA_ARGS__);      \
+        _logMessage(::_variadicExpand(MESSAGE, ##__VA_ARGS__).c_str(), \
+                    __FILE__, __LINE__, __FUNCTION__)
+#else
+    #define LOG(MESSAGE, ...)                                          \
+        _logMessage(::_variadicExpand(MESSAGE, ##__VA_ARGS__).c_str(), \
+                    __FILE__, __LINE__, __FUNCTION__)
+#endif // defined(DEBUG)
