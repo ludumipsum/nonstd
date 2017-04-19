f367b308f377a484731908499232e99cb86d64d0
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2017-04-19 17:05:44 -0400

    Change comment style in logging.h

--- common/logging.h
+++ common/logging.h
@@ -1,11 +1,10 @@
 /* Loggin' Utilities
-   =================
-
-   Utility functions and macros for logging, variadic printf-expansion, and
-   similar chores.
-
-   https://www.youtube.com/watch?v=siwpn14IE7E
-*/
+ * =================
+ * Utility functions and macros for logging, variadic printf-expansion, and
+ * similar chores.
+ *
+ * https://www.youtube.com/watch?v=siwpn14IE7E
+ */
 
 #pragma once
 
@@ -14,13 +13,13 @@
 
 
 /* Variadic Message Format Expander
-   --------------------------------
-   _variadicExpand allows the `MESSAGE` parameter that's passed into `LOG` take
-   the form of a format string.
-   Modified from Erik Aronesty's answer of this SO question:
-   http://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
-   but also: http://stackoverflow.com/questions/7825648/vscprintf-equivalent-on-android/
-*/
+ * --------------------------------
+ * _variadicExpand allows the `MESSAGE` parameter that's passed into `LOG` take
+ * the form of a format string.
+ * Modified from Erik Aronesty's answer of this SO question:
+ * http://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
+ * but also: http://stackoverflow.com/questions/7825648/vscprintf-equivalent-on-android/
+ */
 inline std::string _variadicExpand(c_cstr message, ...)
 {
     va_list format_args;
@@ -41,9 +40,9 @@ inline std::string _variadicExpand(c_cstr message, ...)
 }
 
 /* Logging Implementation
-   ----------------------
-   Actually emit text from calls made with LOG(...)
-*/
+ * ----------------------
+ * Actually emit text from calls made with LOG(...)
+ */
 inline i32 _logMessage(c_cstr    message,
                        c_cstr    file,
                        i32 const line,
@@ -66,10 +65,10 @@ inline i32 _logMessage(c_cstr    message,
 }
 
 /* Logging Macro Shorthand
-   -----------------------
-   If you want to log a thing, you almost 100% definitely certainly want to
-   probably use this one.
-*/
+ * -----------------------
+ * If you want to log a thing, you almost 100% definitely certainly want to
+ * probably use this one.
+ */
 #define LOG(MESSAGE, ...)                                          \
     _logMessage(::_variadicExpand(MESSAGE, ##__VA_ARGS__).c_str(), \
                 __FILE__,                                          \
