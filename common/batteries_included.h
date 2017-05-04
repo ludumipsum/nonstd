1c3513d6f87381f769e6e998b6a424305c167a69
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2017-05-04 14:02:10 -0400

    Add a value-category deducer / printer

--- common/batteries_included.h
+++ common/batteries_included.h
@@ -395,3 +395,24 @@ struct N2TypeName {
 };
 
 #endif
+
+
+/* Value Category Checker
+ * ======================
+ * Like it says on the tin.
+ * Taken nearly directly from http://stackoverflow.com/questions/16637945
+ */
+template<typename T> struct value_category {
+    static constexpr auto value = "prvalue";
+};
+
+template<typename T> struct value_category<T&> {
+    static constexpr auto value = "lvalue";
+};
+
+template<typename T> struct value_category<T&&> {
+    static constexpr auto value = "xvalue";
+};
+
+// Double parens for ensuring we inspect an expression, not an entity
+#define VALUE_CATEGORY(expr) value_category<decltype((expr))>::value
