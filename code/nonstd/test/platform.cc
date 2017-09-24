e1370e24e16954ccb588c6ad0250a69905b7669e
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2017-09-24 15:19:04 -0400

    Add a test to make sure exceptions work

---
+++ code/nonstd/test/platform.cc
@@ -0,0 +1,20 @@
+/** Platform Smoke Tests
+ *  ====================
+ *  To make sure C++ works the way we think it should work.
+ */
+
+#include <testrunner/testrunner.h>
+
+namespace nonstd_test {
+namespace platform {
+
+TEST_CASE("Exception handlers") {
+    SECTION("should throw and catch") {
+        REQUIRE_THROWS([]() {
+            throw std::logic_error("Yeah, this is an error");
+        }());
+    }
+}
+
+} /* namespace nonstd_test */
+} /* namespace platform */
