8af82a2142b198a3c53f06a31d04c70240e3e74a
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2017-10-21 15:47:04 -0400

    Improve equal_to
    Give it more correct noexcept semantics, add some more tests, and
    leverage std::equal_to in the general case (we can and shouldstill
    overload freely).

--- code/nonstd/std_ish/compare.h
+++ code/nonstd/std_ish/compare.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <cstring>
+#include <functional>
 
 #include "nonstd/preprocessor/homogenize.h"
 #include "nonstd/core/primitive_types.h"
@@ -14,20 +15,23 @@
 
 namespace nonstd {
 
-template<typename L, typename R>
-constexpr FORCEINLINE auto equal_to(L left, R right)
--> ENABLE_IF_DTYPE(!(IS_ARITHMETIC(L) && IS_ARITHMETIC(R)), bool) {
-    return left == right;
+/** `equal_to` Free Function
+ *  ------------------------
+ *  This function steals a lot of its functionality from the <functional>
+ *  std::equal_to struct. Noteably, though, we define a free-function, rather
+ *  than a function object, s.t. specializations can be created out of scope by
+ *  defining *  non-template overloads.
+ */
+template <typename L, typename R>
+constexpr bool equal_to(L&& lhs, R&& rhs)
+noexcept(noexcept(std::forward<L>(lhs) == std::forward<R>(rhs))) {
+    return std::equal_to<>()(std::forward<L>(lhs), std::forward<R>(rhs));
 }
 
-template<typename L, typename R>
-constexpr FORCEINLINE auto equal_to(L left, R right) noexcept
--> ENABLE_IF_DTYPE( (IS_ARITHMETIC(L) && IS_ARITHMETIC(R)), bool) {
-    return left == right;
-}
-
-FORCEINLINE bool equal_to(c_cstr left, c_cstr right) {
-    return (strcmp(left, right) == 0);
+// NB. Not constexpr, because `strcmp`. It's noexcept because segfaults are
+//     possible, and we _might_ get segv exceptions rather than terminations.
+inline bool equal_to(c_cstr lhs, c_cstr rhs) {
+    return (strcmp(lhs, rhs) == 0);
 }
 
 // Additional noexcept overloads may be added with ex;
--- code/nonstd/std_ish/test/compare.cc
+++ code/nonstd/std_ish/test/compare.cc
@@ -16,6 +16,14 @@ namespace compare {
 using nonstd::compare;
 using nonstd::equal_to;
 
+struct BadComp {
+    int x;
+
+    bool operator==(BadComp const & other) noexcept(false) {
+        if (x == 0) { throw std::runtime_error("What a bad class"); }
+        return x == other.x;
+    }
+};
 
 static_assert( noexcept(compare((u64)1,(u64)2)),
                "nonstd::compare(u64,u64) is correctly noexcept");
@@ -30,9 +38,16 @@ static_assert( noexcept(equal_to((u64)1,(u32)2)),
                "nonstd::equal_to(u64,u32) is correctly noexcept");
 static_assert(!noexcept(equal_to("a","b")),
                "nonstd::equal_to(c_int,c_int) cannot be noexcept");
+static_assert(!noexcept(equal_to(BadComp{1}, BadComp{2})),
+               "nonstd::equal_to(BadComp,BadComp) is explicitly not noexcept");
 
 TEST_CASE("Comparison utilities") {
     SECTION("should correctly compare values") {
+        // strcmp baseline
+        REQUIRE(strcmp("1", "1") == 0);
+        REQUIRE(strcmp("1", "2") <  0);
+        REQUIRE(strcmp("2", "1") >  0);
+
         REQUIRE(compare(1, 1) == 0);
         REQUIRE(compare(1, 2) <  0);
         REQUIRE(compare(2, 1) >  0);
