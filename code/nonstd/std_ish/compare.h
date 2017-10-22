45ccc8565380864502e675a344f4612bfe32cced
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2017-10-22 18:52:52 -0400

    Update comments
    Okay. This has got to be the final update. Right?

--- code/nonstd/std_ish/compare.h
+++ code/nonstd/std_ish/compare.h
@@ -18,7 +18,13 @@ namespace nonstd {
  *  ------------------------
  *  Relies on the given types having correctly overloaded `operator==` members.
  *  Correctly deduces noexcept using the `noexcept(noexcept(...))` construct.
- *  Using std::forward to manipulate the types because... that's what libc++ do?
+ *
+ *  Additional noexcept overloads may be added with ex;
+ *      template<typename L, typename R>
+ *      constexpr inline bool equal_to(Optional<L> lhs, Optional<R> rhs)
+ *      noexcept {
+ *          return lhs == rhs;
+ *      }
  */
 template <typename L, typename R>
 constexpr bool equal_to(L const & lhs, R const & rhs)
@@ -31,19 +37,20 @@ inline bool equal_to(c_cstr lhs, c_cstr rhs) {
     return (strcmp(lhs, rhs) == 0);
 }
 
-// Additional noexcept overloads may be added with ex;
-//     template<typename L, typename R>
-//     constexpr inline bool equal_to(Optional<L> lhs, Optional<R> rhs)
-//     noexcept {
-//         return lhs == rhs;
-//     }
-
 /** `compare` Free Function
  *  -----------------------
  *  Relies on the given types having correctly overloaded `operator<` and
  *  `operator>` members. Correctly deduces noexcept using the
  * `noexcept(noexcept(...))` construct.
- *  Using std::forward to manipulate the types because... that's what libc++ do?
+ *
+ *  Additional noexcept overloads may be added with ex;
+ *     template<typename L, typename R>
+ *     constexpr inline bool compare(Optional<L> lhs, Optional<R> rhs)
+ *     noexcept {
+ *         if (lhs > rhs) { return  1; } else
+ *         if (lhs < rhs) { return -1; }
+ *         return 0;
+ *     }
  */
 template <typename L, typename R>
 constexpr int compare(L const & lhs, R const & rhs)
@@ -59,14 +66,5 @@ noexcept(noexcept(lhs < rhs) &&
 inline int compare(c_cstr lhs, c_cstr rhs) {
     return strcmp(lhs, rhs);
 }
-
-// Additional noexcept overloads may be added with ex;
-//    template<typename L, typename R>
-//    constexpr inline bool compare(Optional<L> lhs, Optional<R> rhs)
-//    noexcept {
-//        if (lhs > rhs) { return  1; } else
-//        if (lhs < rhs) { return -1; }
-//        return 0;
-//    }
 
 } /* namespace nonstd */
