b63fb127b41ffbb7950d9ff95d817fdf8815c884
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2017-12-08 13:59:05 -0500

    Build out BufferedValue a whole bunch

--- code/nonstd/memory/buffered_value.h
+++ code/nonstd/memory/buffered_value.h
@@ -13,41 +13,77 @@ namespace nonstd {
 
 template <typename T>
 struct BufferedValue {
+    static_assert(!IS_REFERENCE(T), "BufferedValues cannot wrap reference types.");
 
-    Buffer * m_buf = nullptr;
+    Buffer * m_buf;
 
-    constexpr BufferedValue() noexcept
-    { }
+
+    inline Buffer * findOrAllocateBuffer(c_cstr name) {
+        auto maybe_buffer = memory::find(name);
+        return maybe_buffer
+             ? *maybe_buffer
+             : memory::allocate(name, sizeof(T));
+    }
+
+    constexpr BufferedValue() noexcept = default;
     constexpr BufferedValue(std::nullptr_t) noexcept
+        : m_buf ( nullptr )
     { }
-    constexpr BufferedValue(Buffer * buf) noexcept
+
+    constexpr explicit BufferedValue(Buffer * buf) noexcept
         : m_buf ( buf )
     { }
-    BufferedValue(c_cstr name)
-        : BufferedValue
-            ( memory::find(name)
-            ? *memory::find(name)
-            : memory::allocate(name, sizeof(T))
-            )
+
+    explicit BufferedValue(c_cstr name)
+        : m_buf ( findOrAllocateBuffer(name) )
     { }
+    BufferedValue(c_cstr name, T const & val)
+        : m_buf ( findOrAllocateBuffer(name) )
+    {
+        set(val);
+    }
+    BufferedValue(c_cstr name, T && val)
+        : m_buf ( findOrAllocateBuffer(name) )
+    {
+        set(std::move(val));
+    }
+
+    BufferedValue(BufferedValue const & other) = default;
+    BufferedValue& operator= (BufferedValue const & other) = default;
+
+    BufferedValue(BufferedValue &&) = delete;
+    BufferedValue& operator= (BufferedValue &&) = delete;
 
 
-    inline BufferedValue& operator= (T val) {
+    inline BufferedValue&  set(T const & val) &  {
         *reinterpret_cast<T*>(m_buf->data) = val;
         return *this;
     }
+    inline BufferedValue&  set(T && val) &  {
+        *reinterpret_cast<T*>(m_buf->data) = std::move(val);
+        return *this;
+    }
+    inline BufferedValue&& set(T const & val) && {
+        return std::move(this->set(val));
+    }
+    inline BufferedValue&& set(T && val) && {
+        return std::move(this->set(std::move(val)));
+    }
 
-    inline T& value() {
+    inline BufferedValue& operator= (T const & val) { return set(val); }
+    inline BufferedValue& operator= (T && val) { return set(std::move(val)); }
+
+    inline T& value() & {
         return *reinterpret_cast<T*>(m_buf->data);
     }
-    inline T& operator* () { return value(); }
-    inline operator T&  () { return value(); }
+    inline T&       operator*   () & { return value(); }
+    inline explicit operator T& () & { return value(); }
 
-    inline T const & value() const {
+    inline T const& value() const& {
         return *reinterpret_cast<T*>(m_buf->data);
     }
-    inline T const & operator* () const { return value(); }
-    inline operator T const &  () const { return value(); }
+    inline T const& operator*         () const& { return value(); }
+    inline explicit operator T const& () const& { return value(); }
 
     /** What Should be the Spaceship Operator
      *  -------------------------------------
@@ -125,6 +161,6 @@ struct BufferedValue {
         return nonstd::compare(lhs.value(), rhs) >= 0;
     }
 
-};
+}; ENFORCE_POD(BufferedValue<int>);
 
 } /* namespace nonstd */
