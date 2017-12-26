147921e394dd1a41e97e74d452e8c06f78eebb28
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2017-12-26 01:42:20 -0500

    Simplify comments

--- code/nonstd/std_ish/cx_math.h
+++ code/nonstd/std_ish/cx_math.h
@@ -37,244 +37,17 @@ using enable_int_if_integral_t = typename std::enable_if_t<std::is_integral_v<T>
  *  so we declare them before the nonstd::cx:: set, but define them after.
  */
 namespace detail {
+
     // Test whether values are within machine epsilon.
-    // Used for algorithm termination.
-    template <typename T>
-    constexpr bool feq(T x, T y);
+    template <typename T> constexpr bool feq(T x, T y);
+
 } /* namespace detail */
 
 
-// -----------------------------------------------------------------------------
-// constexpr math functions
+/** Constexpr Math Utilities
+ *  ------------------------
+ */
 
-// -----------------------------------------------------------------------------
-// is INFINITE / is NaN functions
-
-// float isinf(float x)
-// double isinf(double x)
-// long double isinf(long double x)
-// double isinf(Integral x)
-
-// float isnan(float x)
-// double isnan(double x)
-// long double isnan(long double x)
-// double isnan(Integral x)
-
-// -----------------------------------------------------------------------------
-// absolute value functions
-
-// float abs(float x);
-// double abs(double x);
-// long double abs(long double x);
-
-// float fabs(float x);
-// double fabs(double x);
-// long double fabs(long double x);
-// double fabs(Integral x);
-
-// -----------------------------------------------------------------------------
-// square root functions
-
-// float sqrt(float x);
-// double sqrt(double x);
-// long double sqrt(long double x);
-// double sqrt(Integral x);
-
-// -----------------------------------------------------------------------------
-// cube root functions
-
-// float cbrt(float x);
-// double cbrt(double x);
-// long double cbrt(long double x);
-// double cbrt(Integral x);
-
-// -----------------------------------------------------------------------------
-// hypotenuse function (returns the square root of the sum of the squares)
-
-// float hypot(float x, float y);
-// double hypot(double x, double y);
-// long double hypot(long double x, long double y);
-// Promoted hypot(Arithmetic1 x, Arithmetic2 y);
-
-// Promotion rules:
-// When either of Arithmetic1 or Arithmetic2 is long double, Promoted is long
-// double. Otherwise Promoted is double.
-
-// -----------------------------------------------------------------------------
-// exponent function (e^x)
-
-// float exp(float x);
-// double exp(double x);
-// long double exp(long double x);
-// double exp(Integral x);
-
-// -----------------------------------------------------------------------------
-// trigonometric functions
-
-// float sin(float x);
-// double sin(double x);
-// long double sin(long double x);
-// double sin(Integral x);
-
-// float cos(float x);
-// double cos(double x);
-// long double cos(long double x);
-// double cos(Integral x);
-
-// float tan(float x);
-// double tan(double x);
-// long double tan(long double x);
-// double tan(Integral x);
-
-// -----------------------------------------------------------------------------
-// inverse trigonometric functions
-
-// float asin(float x);
-// double asin(double x);
-// long double asin(long double x);
-// double asin(Integral x);
-
-// float acos(float x);
-// double acos(double x);
-// long double acos(long double x);
-// double acos(Integral x);
-
-// float atan(float x);
-// double atan(double x);
-// long double atan(long double x);
-// double atan(Integral x);
-
-// float atan2(float x, float y);
-// double atan2(double x, double y);
-// long double atan2(long double x, long double y);
-// Promoted atan2(Arithmetic1 x, Arithmetic2 y);
-
-// -----------------------------------------------------------------------------
-// rounding functions (long double versions exist only for C++14)
-
-// float floor(float x);
-// double floor(double x);
-// long double floor(long double x);
-// double floor(Integral x);
-
-// float ceil(float x);
-// double ceil(double x);
-// long double ceil(long double x);
-// double ceil(Integral x);
-
-// float trunc(float x);
-// double trunc(double x);
-// long double trunc(long double x);
-// double trunc(Integral x);
-
-// float round(float x);
-// double round(double x);
-// long double round(long double x);
-// double round(Integral x);
-
-// -----------------------------------------------------------------------------
-// remainder functions (long double versions exist only for C++14)
-
-// float fmod(float x, float y);
-// double fmod(double x, double y);
-// long double fmod(long double x, long double y);
-// Promoted fmod(Arithmetic1 x, Arithmetic2 y);
-
-// float remainder(float x, float y);
-// double remainder(double x, double y);
-// long double remainder(long double x, long double y);
-// Promoted remainder(Arithmetic1 x, Arithmetic2 y);
-
-// -----------------------------------------------------------------------------
-// max/min functions
-
-// float fmax(float x, float y);
-// double fmax(double x, double y);
-// long double fmax(long double x, long double y);
-// Promoted fmax(Arithmetic1 x, Arithmetic2 y);
-
-// float fmin(float x, float y);
-// double fmin(double x, double y);
-// long double fmin(long double x, long double y);
-// Promoted fmin(Arithmetic1 x, Arithmetic2 y);
-
-// float fdim(float x, float y);
-// double fdim(double x, double y);
-// long double fdim(long double x, long double y);
-// Promoted fdim(Arithmetic1 x, Arithmetic2 y);
-
-// -----------------------------------------------------------------------------
-// logarithm functions
-
-// float log(float x);
-// double log(double x);
-// long double log(long double x);
-// double log(Integral x);
-
-// float log10(float x);
-// double log10(double x);
-// long double log10(long double x);
-// double log10(Integral x);
-
-// float log2(float x);
-// double log2(double x);
-// long double log2(long double x);
-// double log2(Integral x);
-
-// -----------------------------------------------------------------------------
-// hyperbolic functions
-
-// float sinh(float x);
-// double sinh(double x);
-// long double sinh(long double x);
-// double sinh(Integral x);
-
-// float cosh(float x);
-// double cosh(double x);
-// long double cosh(long double x);
-// double cosh(Integral x);
-
-// float tanh(float x);
-// double tanh(double x);
-// long double tanh(long double x);
-// double tanh(Integral x);
-
-// -----------------------------------------------------------------------------
-// inverse hyperbolic functions
-
-// float asinh(float x);
-// double asinh(double x);
-// long double asinh(long double x);
-// double asinh(Integral x);
-
-// float acosh(float x);
-// double acosh(double x);
-// long double acosh(long double x);
-// double acosh(Integral x);
-
-// float atanh(float x);
-// double atanh(double x);
-// long double atanh(long double x);
-// double atanh(Integral x);
-
-// -----------------------------------------------------------------------------
-// power function
-
-// float pow(float x, float y);
-// double pow(double x, double y);
-// long double pow(long double x, long double y);
-// Promoted pow(Arithmetic1 x, Arithmetic2 y);
-
-// -----------------------------------------------------------------------------
-// Gauss error function
-
-// float erf(float x);
-// double erf(double x);
-// long double erf(long double x);
-// double erf(Integral x);
-
-
-//----------------------------------------------------------------------------
 // Test if a value is INFINITE.
 template <typename FloatingPoint,
           enable_int_if_floating_point_t<FloatingPoint> = 0>
@@ -287,7 +60,6 @@ constexpr bool isinf(Integral x) {
     return isinf(static_cast<double>(x));
 }
 
-//----------------------------------------------------------------------------
 // Test if a value is NaN.
 template <typename FloatingPoint,
           enable_int_if_floating_point_t<FloatingPoint> = 0>
@@ -301,7 +73,6 @@ constexpr bool isnan(Integral x) {
     return isnan(static_cast<double>(x));
 }
 
-//----------------------------------------------------------------------------
 // Take the absolute value.
 template <typename FloatingPoint,
           enable_int_if_floating_point_t<FloatingPoint> = 0>
@@ -311,7 +82,6 @@ constexpr FloatingPoint abs(FloatingPoint x) {
          : x;
 }
 
-//----------------------------------------------------------------------------
 // Take the absolute value.
 template <typename FloatingPoint,
           enable_int_if_floating_point_t<FloatingPoint> = 0>
