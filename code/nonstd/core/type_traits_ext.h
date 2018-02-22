45ccd31abbf1ad03a9c3621085378931afe2133a
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2018-02-21 23:33:32 -0500

    Whitespace, naming in type_traits_ext.h
    The major changes are moving away from leading `_`s in names. The
    rules regarding what kinds of names are reserved by the C++
    standard / library are weird and hard to remember, so I'd rather
    just avoid the issue.

--- code/nonstd/core/type_traits_ext.h
+++ code/nonstd/core/type_traits_ext.h
@@ -10,12 +10,13 @@
 
 
 /** Extensions
- *  ----------
+ *  ==========
  */
 
 namespace nonstd {
 
 /** have_same_type
+ *  --------------
  *  Check if, essentially, ::std::is_same_v<decltype(L), decltype(R)>
  */
 template <typename L, typename R>
@@ -24,6 +25,7 @@ constexpr bool have_same_type(L&& l, R&& r) noexcept {
 }
 
 /** have_same_decayed_type
+ *  ----------------------
  *  Check if the decayed types of the left and right arguments are identical
  */
 template <typename L, typename R>
@@ -31,8 +33,8 @@ constexpr bool have_same_decayed_type(L&& l, R&& r) noexcept {
     return ::std::is_same_v<std::decay_t<L>, std::decay_t<R>>;
 }
 
-
 /** is_reference_wrapper
+ *  --------------------
  *  Shorthand check for whether or not a type argument is an STL
  *  reference_wrapper (a utility class used to hold references kinda like
  *  pointers when manipulating containers / contained objects).
@@ -45,6 +47,7 @@ template <typename T>
 inline constexpr bool is_reference_wrapper_v = is_reference_wrapper<T>::value;
 
 /** remove_cvref
+ *  -------------
  *  If the type `T` is a reference type, provides the member type `type` which
  *  is the type referred to by `T` with its topmost cv-qualifiers removed.
  *  Otherwise `type` is `T` with its topmost cv-qualifiers removed.
@@ -58,8 +61,8 @@ struct remove_cvref {
 template <typename T>
 using remove_cvref_t = typename remove_cvref<T>::type;
 
-
 /** is_swappable && is_nothrow_swappable
+ *  -----------------------------------
  *  If the expressions `swap(std::declval<T>(), std::declval<T>())` is
  *  well-formed in an unevaluated context after using std::swap; provides the
  *  member constant `value` equal to `true`. Otherwise, `value` is `false`.
@@ -69,12 +72,12 @@ using remove_cvref_t = typename remove_cvref<T>::type;
  *
  *  This implementation was largely taken from the GCC 7.2 libstdc++ v3.
  */
-namespace __is_swappable_details  {
+namespace detail::is_swappable_  {
 
     struct do_impl {
         template < typename T
-                 , typename _ = decltype(std::swap(std::declval<T&>(),
-                                                   std::declval<T&>()))>
+                 , typename a_ = decltype(std::swap(std::declval<T&>(),
+                                                    std::declval<T&>()))>
         static std::true_type test(int);
 
         template <typename>
@@ -103,11 +106,11 @@ namespace __is_swappable_details  {
     template <typename T>
     using nothrow_t = typename nothrow<T>::type;
 
-} /* namespace __is_swappable_details */
+} /* namespace detail::is_swappable_ */
 
 template <typename T>
 struct is_swappable
-    : public __is_swappable_details::impl_t<T>
+    : public detail::is_swappable_::impl_t<T>
 { };
 template <typename T>
 inline constexpr bool is_swappable_v = is_swappable<T>::value;
@@ -115,7 +118,7 @@ inline constexpr bool is_swappable_v = is_swappable<T>::value;
 
 template <typename T>
 struct is_nothrow_swappable
-    : public __is_swappable_details::nothrow_t<T>
+    : public detail::is_swappable_::nothrow_t<T>
 { };
 template <typename T>
 inline constexpr bool is_nothrow_swappable_v = is_nothrow_swappable<T>::value;
@@ -124,11 +127,12 @@ inline constexpr bool is_nothrow_swappable_v = is_nothrow_swappable<T>::value;
 
 
 /** Assertions
- *  ----------
+ *  ==========
  *  Macros that cause compile-time errors when we build non-compliant datatypes.
  */
 
 /** ENFORCE
+ *  -------
  *  General purpose enforcement macro to simplify one-off static_asserts.
  */
 #define ENFORCE(condition) \
@@ -136,6 +140,7 @@ inline constexpr bool is_nothrow_swappable_v = is_nothrow_swappable<T>::value;
 
 
 /** ENFORCE_POD
+ *  -----------
  *  Enforce the given type is a POD datatype.
  */
 #define ENFORCE_POD(T) \
@@ -144,6 +149,7 @@ inline constexpr bool is_nothrow_swappable_v = is_nothrow_swappable<T>::value;
     static_assert(std::is_standard_layout_v<T>, "Type '" STRING(T) "' was marked as Plain Old Data, but is not standard layout. Defined near [" STRING(__FILE__) ":" STRING(__LINE__) "]")
 
 /** ENFORCE_SIZE (and friends)
+ *  ------------ - - - - - - -
  *  Enforce the given type is exactly / under / above / within the range of the
  *  given size(s).
  */
@@ -153,12 +159,14 @@ inline constexpr bool is_nothrow_swappable_v = is_nothrow_swappable<T>::value;
     static_assert(sizeof(T) <= bytes, "Type '" STRING(T) "' is the wrong size (it is required to be at most " STRING(bytes) " bytes).")
 
 /** ENFORCE_IS_SAME
+ *  ---------------
  *  Enforce the given types are the same, as shown by `std::is_same_v`.
  */
 #define ENFORCE_IS_SAME(LEFT, RIGHT) \
     static_assert(::std::is_same_v<LEFT, RIGHT>, "'" STRING(LEFT) "' doesn't have the same type as '" STRING(RIGHT) "'.")
 
 /** ENFORCE_HAVE_SAME_TYPE
+ *  ----------------------
  *  Enforce the given objects have the same type, as shown by `decltype`.
  */
 #define ENFORCE_HAVE_SAME_TYPE(LEFT, RIGHT) \
