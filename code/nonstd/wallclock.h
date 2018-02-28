49ef6cd91345a5ba4a98d3227d7c955dbf6838d2
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2018-02-28 13:34:21 -0500

    Move wallclock from an API module, to a nonstd struct

---
+++ code/nonstd/wallclock.h
@@ -0,0 +1,37 @@
+/** A Clock on the Wall
+ *  ===================
+ *  Uses std::chrono for all of its timing durations, and std::thread for sleep.
+ */
+#pragma once
+
+#include <chrono>
+#include <thread>
+
+using namespace std::literals::chrono_literals;
+
+
+namespace nonstd {
+
+struct wallclock {
+    /** Query the system high-frequency monotonic clock. */
+    inline static std::chrono::nanoseconds now() noexcept {
+        return std::chrono::steady_clock::now().time_since_epoch();
+    }
+    inline static std::chrono::microseconds now_us() noexcept {
+        return std::chrono::round<std::chrono::microseconds>(now());
+    }
+    inline static std::chrono::milliseconds now_ms() noexcept {
+        return std::chrono::round<std::chrono::milliseconds>(now());
+    }
+    inline static std::chrono::seconds now_sec() noexcept {
+        return std::chrono::round<std::chrono::seconds>(now());
+    }
+
+    /** Sleep the calling thread for the provided number of milliseconds */
+    template <typename Rep, typename Period = std::ratio<1>>
+    static void delay(std::chrono::duration<Rep, Period> const & duration) {
+        std::this_thread::sleep_for(duration);
+    }
+};
+
+} /* namespace nonstd */
---
+++ code/nonstd/wallclock.test.cc
@@ -0,0 +1,41 @@
+/** System Time (Standalone) Tests
+ *  ==============================
+ *  This library is tiny, so there's not a lot to consider in testing it --
+ *  basically all we can do is run it, sleep, and run it again, and confirm the
+ *  numbers are different. Note, though, that even that isn't necessarily a
+ *  guaranteed property of the high precision clock (the one we use). There are
+ *  "safer" clocks available, for example ones enforcing monotonicity, but
+ *  because we prefer greater precision moment to moment at the expense of
+ *  possible occasional skips, we don't use them.
+ *
+ *  Our clock, therefore, reports time in nanoseconds since an arbitrary epoch.
+ *  It may run forwards, backwards, or not at all. Any u64 is a viable output.
+ *
+ *  Given the total lack of guarantees provided by the clock we rely on, we just
+ *  test a few sanity checks — stuff that isn't technically a contract violation,
+ *  but that would be problematic for us if it turned out wrong.
+ */
+
+#include "nonstd/wallclock.h"
+#include <testrunner/testrunner.h>
+
+
+namespace test {
+namespace wallclock {
+
+using nonstd::wallclock;
+
+TEST_CASE("System timer", "[time][standalone]") {
+    SECTION("returns a nonzero u64") {
+        REQUIRE(wallclock::now() != std::chrono::nanoseconds::zero());
+    }
+    SECTION("returns different numbers at different times") {
+        auto point_a = wallclock::now();
+        wallclock::delay(1.5s);
+        auto point_b = wallclock::now();
+        REQUIRE(point_a != point_b);
+    }
+}
+
+} /* namespace wallclock */
+} /* namespace test */
