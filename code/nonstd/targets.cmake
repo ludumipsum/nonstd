b0577860f330a776c6ad4df219769f108b64e7c4
Alex Berghage <aberghage@gmail.com>
2018-03-19 23:44:51 -0400

    Automatic paths relative to targets.cmake
    This means you no longer have to specify the PATH
    variable in tfe_* macros, and if you do, you're
    setting it relative to the targets.cmake file
    and not the root.

--- code/nonstd/targets.cmake
+++ code/nonstd/targets.cmake
@@ -1,11 +1,16 @@
 # Targets listing for nonstd
 
+tfe_library(
+    NAME nonstd-windows
+    HEADERS windows.h
+)
+
 # TODO: Pull core out to a separate library
 tfe_library(
     NAME nonstd
     HEADERS
         # TODO: Fix the header list too
-        nonstd/nonstd.h
+        nonstd.h
     DEPENDS
         # TODO: Fix deps for this target
         Boost::boost fmt::fmt
