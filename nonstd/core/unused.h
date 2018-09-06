1553c2c97fe0ed9bd6b6d5c930d1d4d6b622ec8a
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2018-09-06 19:43:32 -0400

    Remove in-source nonstd

--- nonstd/core/unused.h
+++
@@ -1,8 +0,0 @@
-/** UNUSED
- *  ======
- *  Utility macro for marking variables deliberately-unused. Nixes warnings.
- */
-
-#pragma once
-
-#define UNUSED(_var) do { (void)(true ? (void)0 : ( (void)(_var) ) ); } while(0)
